2ff280242a6ca4dee2c078a0635194b39873b093
rutapon <rutapon@hotmail.com>
2016-08-15 19:02:58 +0700

    d

--- web/js/app.js
+++ web/js/app.js
@@ -18,9 +18,10 @@
 
     if (window.location.protocol == 'https:') {
         protocol = 'wss:';
+        var wsClient = app.wsClient = new NwWsClient(protocol + '//' + host + port, { secure: true });
+    } else {
+        var wsClient = app.wsClient = new NwWsClient(protocol + '//' + host + port);
     }
-    var wsClient = app.wsClient = new NwWsClient(protocol + '//' + host  + port);
-
     var stockMethod = app.stockMethod = new NwStockServiceConn(wsClient);
     wsClient.setOnConnectEventListener(function (socket) {
         var id = wsClient.getId();
