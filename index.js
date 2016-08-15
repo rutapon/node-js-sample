da04bd3c6a7842f85113ede42a7831d91fcd8684
rutapon <rutapon@hotmail.com>
2016-08-15 16:38:40 +0700

    d

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
-var express = require('express')
-var app = express()
+//var express = require('express')
+//var app = express()
+
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
 
 app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+//app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+//app.get('/', function(request, response) {
+//    response.send('Hello World! by newww');
+//})
+
+
+app.get('/', function (req, res) {
+    response.send('Hello World! by newww');
+  //res.sendfile('index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+//http.listen(3000, function(){
+//  console.log('listening on *:3000');
+//});
+
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
