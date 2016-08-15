3b4c6791c1c0d34dae8ec398dcf5071e3ce56bed
rutapon <rutapon@hotmail.com>
2016-08-16 05:33:01 +0700

    d

---
+++ public/service_conn/NwStockServiceConn.js
@@ -0,0 +1,95 @@
+﻿/// <reference path="../../lib/socket.io-1.0.6.js" />
+/// <reference path="../../Lib/step/step.js" />
+/// <reference path="../lib/async/async.js" />
+/// <reference path="../../NwLib/NwLib.js" />
+
+
+/// <reference path="../NwConn/NwConn.js" />
+
+
+
+(function (context, undefined) {
+    //#region requre
+
+    if (typeof module !== "undefined") {
+
+        NwLib = require('../NwLib/NwLib.js');
+        Class = NwLib.Nwjsface.Class;
+
+        //NwDataMsgObj = require('../NwUtil/NwDataMsgObj.js');
+
+        ////http = require('http');
+
+        //NwConn = require('../NwConn/NwConn.js');
+
+        //_ = require('../Lib/underscore/underscore.js');
+
+    } else {
+
+    }
+
+    //#endregion
+    var NwStockServiceConn = Class(function () {
+
+        return {
+            //$singleton: true,
+            wsClient: {},
+
+            constructor: function (wsClient) {
+                this.wsClient = wsClient;
+            },
+
+            getAllStockName: function (cb) {
+                this.wsClient.callService('getAllStockName', {}, cb);
+            },
+
+            getAllProducts: function (stockName, cb) {
+                this.wsClient.callService('getAllProducts', { stockName: stockName }, cb);
+            },
+
+            findeProductStartWith: function (stockName, findWord, limit, cb) {
+                this.wsClient.callService('findeProductStartWith', { stockName: stockName, findWord: findWord, limit: limit }, cb);
+            },
+            insertProduct: function (stockName, insertObj, cb) {
+                insertObj.stockName = stockName;
+                this.wsClient.callService('insertProduct', insertObj, cb);
+            },
+            updateProduct: function (stockName, updateObj, cb) {
+                updateObj.stockName = stockName;
+                this.wsClient.callService('updateProduct', updateObj, cb);
+            },
+            deleteProduct: function (stockName, code, cb) {
+                this.wsClient.callService('deleteProduct', { stockName: stockName, code: code }, cb);
+            },
+            insertSupplyLog: function (dataObj, cb) {
+                this.wsClient.callService('insertSupplyLog', dataObj, cb);
+            },
+            updateSupplyLog: function (dataObj, cb) {
+                this.wsClient.callService('updateSupplyLog', dataObj, cb);
+            },
+            findeSupplyLog: function (dataObj, cb) {
+                this.wsClient.callService('findeSupplyLog', dataObj, cb);
+            },
+            checkForInsertSupplyLog: function (dataObj, cb) {
+                this.wsClient.callService('checkForInsertSupplyLog', dataObj, cb);
+            },
+            getAllSupplyLog: function (dataObj, cb) {
+                this.wsClient.callService('getAllSupplyLog', dataObj, cb);
+            },
+            insertImportProduct: function (dataObj, cb) {
+                this.wsClient.callService('insertImportProduct', dataObj, cb);
+            },
+            getImportProductInPeriod: function (dataObj, cb) {
+                this.wsClient.callService('getImportProductInPeriod', dataObj, cb);
+            },
+        };
+    });
+
+    if (typeof module !== "undefined" && module.exports) {                       // NodeJS/CommonJS
+        module.exports = NwStockServiceConn;
+    } else {
+
+        context.NwStockServiceConn = NwStockServiceConn;
+    }
+
+})(this);
---
+++ public/test.js
@@ -0,0 +1,32 @@
+﻿/// <reference path="NwLib/NwLib.js" />
+/// <reference path="lib/jquery/jquery-2.1.1.js" />
+/// <reference path="lib/backbone/backbone.js" />
+/// <reference path="lib/underscore/underscore.js" />
+
+
+(function () {
+    var host = window.location.hostname;
+    var port = window.location.port;
+    var protocol = 'ws:';
+    //var host = 'localhost';
+    //var host = 'newww.dyndns.org';
+    //alert(window.location.protocol + window.location.port);
+
+    if (window.location.protocol == 'https:') {
+        protocol = 'wss:';
+        var wsClient = app.wsClient = new NwWsClient(protocol + '//' + host + ":" + port, { secure: true });
+    } else {
+        var wsClient = app.wsClient = new NwWsClient(protocol + '//' + host + ":" + port);
+    }
+    var stockMethod = app.stockMethod = new NwStockServiceConn(wsClient);
+    wsClient.setOnConnectEventListener(function (socket) {
+        var id = wsClient.getId();
+        console.log('onConnect ' + id);
+    });
+
+    wsClient.setOnDisconnectEventListener(function myfunction() {
+
+    });
+
+ 
+})();
